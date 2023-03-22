1cb86364be9053c452ef49b7b8232730563e6c18
kitou88C39 <kanfutrooper@icloud.com>
2023-03-22 20:31:49 +0900

    second commit

---
+++ config/db.config.js
@@ -0,0 +1,20 @@
+const HOST = 'localhost';
+const USER = 'postgres';
+const PASSWORD = 'Welcome123';
+const DB = 'csvdb';
+const dialect = 'postgres';
+const pool = {
+  max: 5,
+  min: 0,
+  acquire: 30000,
+  idle: 10000,
+};
+
+export default {
+  HOST,
+  USER,
+  PASSWORD,
+  DB,
+  dialect,
+  pool,
+};
